1dca3e38df04fdddde07fb9b74588df5c9f64bfd
Xyves <xodrixx@protonmail.com>
2024-11-16 22:38:15 +0100

    style: Add new styling to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,16 @@ import 'primeicons/primeicons.css'
 
 export default function Footer() {
   return (
-    <button className="absolute">
-      <i
-        className="pi pi-github 
+    <footer className="fixed bottom-0 left-0 m-8 flex w-52 items-center justify-center bg-[#1B1725] px-10 py-8 text-white">
+      <button className="inline-block  ">
+        <i
+          className="pi pi-github block pr-2 text-2xl
 "
-      ></i>
-      <span className="absolute uppercase">github</span>
-    </button>
+        ></i>
+        <span className=" text-md inline-block font-footer uppercase">
+          Github
+        </span>
+      </button>
+    </footer>
   )
 }
