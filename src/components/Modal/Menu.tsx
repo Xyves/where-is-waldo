9c5d631f45941720088551fc9da399698904dd37
Xyves <xodrixx@protonmail.com>
2024-11-20 21:46:40 +0100

    feat: Create modal menu

---
+++ src/components/Modal/Menu.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function ModalMenu() {
+  return (
+    <ul className="grid h-64 w-72 grid-cols-1 grid-rows-3 bg-blue-900 p-4">
+      <form action="">
+        input
+        <li className="flex items-center  rounded-lg bg-red-300 ">
+          <div className=" h-16 w-1/3 bg-yellow-300"></div>
+          <p className=" pl-6 text-center">xaxaa</p>
+        </li>
+        <li className="flex items-center  rounded-lg bg-red-300">
+          <div className=" h-16 w-1/3 bg-yellow-300"></div>
+          <p className=" pl-6 text-center">xaxaa</p>
+        </li>
+        <li className="flex items-center  rounded-lg bg-red-300">
+          <div className=" h-16 w-1/3 bg-yellow-300"></div>
+          <p className=" pl-6 text-center">xaxaa</p>
+        </li>
+      </form>
+    </ul>
+  )
+}
