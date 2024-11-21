7cd44a74550dc2e2edbe05eb03e51cd994dc07a3
Xyves <xodrixx@protonmail.com>
2024-11-21 22:07:04 +0100

    feat: Create character list component

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
-export default function CharacterList() {
-  return <div className="absolute">CharacterList</div>
+export default function CharacterList({ characters }) {
+  return (
+    <div className="fixed right-0 top-0 mr-6 mt-6 flex *:rounded-lg ">
+      <li className="mx-3 size-24 bg-red-300"></li>
+      <li className="mx-3 size-24 bg-blue-300"></li>
+      <li className="mx-3 size-24 bg-yellow-300"></li>
+    </div>
+  )
 }
