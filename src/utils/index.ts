dcea16dd21b9f703ccfb0c06bf55762fb76c9512
Xyves <xodrixx@protonmail.com>
2024-11-26 21:28:33 +0100

    Create calculating coords logic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,16 @@
 export function generateCharacters() {}
+export function calculateCoords(oldCoords: Coords) {
+  const dpr = window.devicePixelRatio || 1
+  const userResolution = {
+    x: window.screen.width * dpr,
+    y: window.screen.height * dpr
+  }
+  const newCoords = {}
+  newCoords.x = (oldCoords.x / userResolution.x) * 1920
+  newCoords.y = (oldCoords.x / userResolution.y) * 1080
+  return newCoords
+}
+type Coords = {
+  x: number
+  y: number
+}
