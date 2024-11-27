c945edf510d7255d9bace8c9d32a495061c4a42b
Xyves <xodrixx@protonmail.com>
2024-11-27 21:38:34 +0100

    Create fetching characters from api function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,21 @@ type Coords = {
   x: number
   y: number
 }
+export async function fetch3Characters() {
+  try {
+    const response = await fetch(
+      'https://where-is-waldo-backend-production-9a1d.up.railway.app',
+      { method: 'GET', headers: { 'Content-Type': 'application/json' } }
+    )
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      console.error(data.message || 'Something went wrong')
+      return null
+    }
+  } catch (e) {
+    console.error('An error occurred:', e)
+    return null
+  }
+}
