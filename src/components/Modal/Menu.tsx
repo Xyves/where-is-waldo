f35134fc9d4504c3ef5a5b7ffaa9a2b1f94aad54
Xyves <xodrixx@protonmail.com>
2024-11-30 21:54:28 +0100

    Update Menu Modal

--- src/components/Modal/Menu.tsx
+++ src/components/Modal/Menu.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import { useCharacters } from 'components/CharactersContext'
+import React, { useState } from 'react'
 
-export default function ModalMenu() {
+export default function ModalMenu({ toggleState }) {
+  const { characters, loading, error } = useCharacters()
+  con
   return (
-    <ul className="grid h-64 w-72 grid-cols-1 grid-rows-3 bg-blue-900 p-4">
+    <ul className=" absolute grid h-64 w-72 grid-cols-1 grid-rows-3 bg-blue-900 p-4">
       <form action="">
-        input
-        <li className="flex items-center  rounded-lg bg-red-300 ">
-          <div className=" h-16 w-1/3 bg-yellow-300"></div>
-          <p className=" pl-6 text-center">xaxaa</p>
-        </li>
-        <li className="flex items-center  rounded-lg bg-red-300">
-          <div className=" h-16 w-1/3 bg-yellow-300"></div>
-          <p className=" pl-6 text-center">xaxaa</p>
-        </li>
-        <li className="flex items-center  rounded-lg bg-red-300">
-          <div className=" h-16 w-1/3 bg-yellow-300"></div>
-          <p className=" pl-6 text-center">xaxaa</p>
-        </li>
+        {characters?.map((character) => (
+          <li
+            className="flex items-center  rounded-lg bg-red-300 "
+            key={character.id}
+            value={character.id}
+          >
+            <img
+              className=" h-16 w-1/3 bg-yellow-300"
+              src={character.character_image}
+            />
+            <p className=" pl-6 text-center">{character.character_name}</p>
+            <button className="bg-red-50 text-red-400" onClick={toggleState}>
+              X
+            </button>
+          </li>
+        ))}
       </form>
     </ul>
   )
