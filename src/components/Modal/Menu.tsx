3db090205543a5ffd524756f84da0c32ac90b19d
Xyves <xodrixx@protonmail.com>
2024-12-02 21:05:12 +0100

    Add closing button to modal

--- src/components/Modal/Menu.tsx
+++ src/components/Modal/Menu.tsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 
 export default function ModalMenu({ toggleState }) {
   const { characters, loading, error } = useCharacters()
-  con
   return (
     <ul className=" absolute grid h-64 w-72 grid-cols-1 grid-rows-3 bg-blue-900 p-4">
       <form action="">
@@ -18,11 +17,14 @@ export default function ModalMenu({ toggleState }) {
               src={character.character_image}
             />
             <p className=" pl-6 text-center">{character.character_name}</p>
-            <button className="bg-red-50 text-red-400" onClick={toggleState}>
-              X
-            </button>
           </li>
         ))}
+        <button
+          className="bg-red-50 text-lg text-red-400"
+          onClick={toggleState}
+        >
+          X
+        </button>
       </form>
     </ul>
   )
