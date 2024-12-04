ab2b0254a0988d53f3ca4139d6ae32fac87fbab3
Xyves <xodrixx@protonmail.com>
2024-12-04 21:53:59 +0100

    feat: Add modal toggle and set coords

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,42 @@
 import Footer from './Footer'
 import Timer from './Timer'
 import CharacterList from './CharacterList'
-import Navbar from './NavBar'
+import Navbar from './Navbar'
+import ModalMenu from './Modal/Menu'
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { CharacterProvider } from './CharactersContext'
+
 function App() {
+  const [isModalOpen, setModalIsOpen] = useState(false)
+  const [coords, setCoords] = useState()
+
+  const toggleState = () => {
+    setModalIsOpen((prevState) => !prevState)
+  }
+  const handleClick = (e) => {
+    const newCoords = [e.pageX, e.pageY]
+    setCoords(newCoords)
+  }
+
   return (
-    <div id="container" className="  ">
-      <Navbar />
-      <div className="game">
-        <img src="main-robotcity.webp" className="h-auto w-full" alt="" />
+    <CharacterProvider>
+      <div
+        className="game  relative"
+        onClick={(e) => {
+          handleClick(e)
+          toggleState()
+        }}
+      >
+        <img src="main-robotcity.webp" className=" h-auto w-full" alt="" />
+        <Navbar />
+        {isModalOpen ? (
+          <ModalMenu toggleState={toggleState} coords={coords} />
+        ) : null}
+
+        <CharacterList />
+        <Footer />
       </div>
-
-      <Footer />
-
-      <div className=""></div>
-    </div>
+    </CharacterProvider>
   )
 }
 
