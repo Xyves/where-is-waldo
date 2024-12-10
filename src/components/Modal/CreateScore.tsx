d0bcd5b5c1f1e0d67c98685bfba955ed9ad4e130
Xyves <xodrixx@protonmail.com>
2024-12-10 22:03:46 +0100

    Implement CreateScore component with form and timer integration

--- src/components/Modal/CreateScore.tsx
+++ src/components/Modal/CreateScore.tsx
@@ -1,5 +1,23 @@
-import React from 'react'
+import TimerClock from 'components/Timer'
+import React, { useContext } from 'react'
+import { postScore } from 'utils'
 
-export default function createScore() {
-  return <div>createScore</div>
+export default function CreateScore() {
+  const { time, timerRunning, setTimerRunning } = useContext(TimerClock)
+  const submitForm = async (e) => {
+    setTimerRunning(false)
+    const username = FormData.get('username')
+    e.preventDefault()
+    await postScore(username, time)
+  }
+  return (
+    <div>
+      {/* w-300 h-600 name  */}
+      <form action="" onSubmit={submitForm}>
+        <label htmlFor="">username</label>
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
 }
