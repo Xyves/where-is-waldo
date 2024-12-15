96b46a6ffd4d53f58b2b2255c8886304dfbe8e89
Xyves <xodrixx@protonmail.com>
2024-12-15 21:21:59 +0100

    Implement character list navbar

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import { useCharacters } from './Context/CharactersContext'
 
-export default function CharacterList({ characters }) {
+export default function CharacterList() {
+  const { characters, loading, error } = useCharacters()
+
+  if (loading) return <p>Loading characters...</p>
+  if (error) return <p>Error: {error}</p>
   return (
-    <div className="fixed right-0 top-0 mr-6 mt-6 flex *:rounded-lg ">
-      <li className="mx-3 size-24 bg-red-300"></li>
-      <li className="mx-3 size-24 bg-blue-300"></li>
-      <li className="mx-3 size-24 bg-yellow-300"></li>
+    <div className="fixed inset-x-0 left-16 top-0  my-5 flex  size-fit items-center justify-center border-2   p-4">
+      <ul className="flex items-center justify-center">
+        {characters?.map((character) => (
+          <li
+            key={character.id}
+            className={`
+              mx-2
+              mb-5 flex items-center justify-center
+               rounded-2xl border-2 border-solid border-[#2E86AB]
+            `}
+          >
+            <figure
+              className={`relative inline-block size-32  rounded-sm bg-[#ffff] ${
+                !character.marked && 'cursor-pointer'
+              }`}
+            >
+              {character.marked && (
+                <span className="absolute inset-0 z-10 flex select-none items-center justify-center text-7xl font-bold text-red-600">
+                  X
+                </span>
+              )}
+              <img
+                src={character.character_image}
+                alt={`image of ${character.character_name}`}
+                className="size-full  opacity-80"
+              />
+              <figcaption className="relative -top-8 h-8 rounded-b-lg text-center text-lg font-bold text-black">
+                {character.character_name}
+              </figcaption>
+            </figure>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
