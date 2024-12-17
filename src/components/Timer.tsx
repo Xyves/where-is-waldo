10215ea5ddb9b6356a16d5573dbeb3313d5db07a
Xyves <xodrixx@protonmail.com>
2024-12-17 21:59:03 +0100

    Import timer context

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,16 +1,10 @@
 import 'primeicons/primeicons.css'
-import { useEffect, useState } from 'react'
-import { createContext } from 'vm'
-const timerContext = createContext()
+import { createContext, useContext, useEffect, useState } from 'react'
+import { TimerProvider, useTimer } from './Context/TimerContext'
 
-export default function TimerClock({ children }) {
-  const [time, setTime] = useState(0)
-  const [timerRunning, setTimerRunning] = useState(false)
-  const contextValue = {
-    time,
-    timerRunning,
-    setTimerRunning
-  }
+export default function Timer({ children }) {
+  const { timerRunning, setTimerRunning, time, setTime } = useTimer()
+
   useEffect(() => {
     let interval = null
     setTimerRunning(true)
@@ -25,21 +19,19 @@ export default function TimerClock({ children }) {
     return () => clearInterval(interval)
   }, [timerRunning])
   return (
-    <timerContext.Provider value={contextValue}>
-      <div className="fixed inset-x-0 top-0 mx-auto mt-8 flex h-16 w-32 items-center justify-center rounded-md bg-[#0FF4C6] px-20 text-[#464F51]">
-        <i className="pi pi-clock mr-2 text-lg"></i>
-        <span className="text-lg">
-          {('0' + (Math.floor(time / 60000) % 60)).slice(-2)}
-        </span>
-        :
-        <span className="text-lg">
-          {('0' + Math.floor((time / 1000) % 60)).slice(-2)}
-        </span>
-        :
-        <span className="text-lg">
-          {('0' + Math.floor((time / 10) % 100)).slice(-2)}
-        </span>
-      </div>
-    </timerContext.Provider>
+    <div className="fixed inset-x-0 top-0 mx-auto mt-8 flex h-16 w-32 items-center justify-center rounded-md bg-[#0FF4C6] px-20 text-[#464F51]">
+      <i className="pi pi-clock mr-2 text-lg"></i>
+      <span className="text-lg">
+        {('0' + (Math.floor(time / 60000) % 60)).slice(-2)}
+      </span>
+      :
+      <span className="text-lg">
+        {('0' + Math.floor((time / 1000) % 60)).slice(-2)}
+      </span>
+      :
+      <span className="text-lg">
+        {('0' + Math.floor((time / 10) % 100)).slice(-2)}
+      </span>
+    </div>
   )
 }
