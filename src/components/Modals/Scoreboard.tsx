b6d067a03a8380c58f4c970aced736e0b6a39071
Xyves <xodrixx@protonmail.com>
2024-12-20 22:29:34 +0100

    Create table in scoreboard component

--- src/components/Modals/Scoreboard.tsx
+++ src/components/Modals/Scoreboard.tsx
@@ -1,5 +1,45 @@
-import React from 'react'
-
 export default function Scoreboard() {
-  return <div>Scoreboard</div>
+  return (
+    <div>
+      Scoreboard
+      <div className="scoreboard">
+        <div className="scoreboardContainer">
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  <div>#</div>
+                </th>
+                <th>
+                  <div>Player</div>
+                </th>
+                <th>
+                  <div>Time</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>tr</tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
+// ;<div>
+//   <div>#</div>
+//   <div>Player</div>
+//   <div>Time</div>
+
+// </div>
+function ScoreRun() {
+  return (
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    // <div className="place"></div>
+    // <div className="player"></div>
+    // <div className="time"></div>
+  )
 }
