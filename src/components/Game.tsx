a1f64eee6dbec248065fece41a3e3855cd4a4fec
Xyves <xodrixx@protonmail.com>
2024-12-21 22:43:21 +0100

    Move App component content to Game

---
+++ src/components/Game.tsx
@@ -0,0 +1,76 @@
+import Footer from './Footer'
+import CharacterList from './CharacterList'
+import Navbar from './Navbar'
+import ModalMenu from './Modals/Menu'
+import { useEffect, useState } from 'react'
+import { CharacterProvider, useCharacters } from './Context/CharactersContext'
+import { calculateCoords } from 'utils'
+import { TimerProvider, useTimer } from './Context/TimerContext'
+import CreateScore from './Modals/CreateScore'
+import Timer from './Timer'
+
+function Game() {
+  const { characters, loading, error, setCharacters } = useCharacters()
+  const [isModalOpen, setModalIsOpen] = useState(false)
+  const { setTimerRunning, timerRunning } = useTimer()
+  const [coords, setCoords] = useState({ x: 0, y: 0 })
+  const [gameActive, setGameActive] = useState(true)
+  const [showDialog, setShowDialog] = useState(false)
+  const [timerActive, setTimerActive] = useState(true)
+  const toggleState = () => {
+    setModalIsOpen((prevState) => !prevState)
+  }
+
+  useEffect(() => {
+    if (gameActive) {
+      setTimerActive(true) // Start the timer when game is active
+    } else {
+      setTimerActive(false)
+      setShowDialog(true)
+    }
+  }, [gameActive, setTimerRunning])
+
+  const handleClick = (e) => {
+    const coordinates = { x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY }
+    const newCoords = calculateCoords(coordinates)
+    setCoords(newCoords)
+  }
+
+  return (
+    <TimerProvider>
+      <CharacterProvider gameActive={gameActive} setGameActive={setGameActive}>
+        <div
+          className="game relative"
+          onClick={(e) => {
+            toggleState()
+            handleClick(e)
+          }}
+        >
+          <img src="main-robotcity.webp" className=" h-auto w-full " alt="" />
+          <Navbar>
+            <Timer timerActive={timerActive} setTimerActive={setTimerActive} />
+            <CharacterList />
+          </Navbar>
+          {isModalOpen ? (
+            <ModalMenu
+              toggleState={toggleState}
+              coords={coords}
+              gameActive={gameActive}
+              setGameActive={setGameActive}
+            />
+          ) : null}
+          <CharacterList gameActive={gameActive} />
+          <Footer />
+        </div>
+        {showDialog && (
+          <CreateScore
+            showDialog={showDialog}
+            setShowDialog={setShowDialog}
+            setGameActive={setGameActive}
+          />
+        )}
+      </CharacterProvider>
+    </TimerProvider>
+  )
+}
+export default Game
