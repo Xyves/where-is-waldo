0247cf5f99b9c5eb27275260675977e3ff285b1f
Xyves <xodrixx@protonmail.com>
2024-12-23 19:04:14 +0100

    Implement create score component

--- src/components/Modals/CreateScore.tsx
+++ src/components/Modals/CreateScore.tsx
@@ -1,34 +1,48 @@
-import TimerClock from 'components/Timer'
-import React, { createContext, useContext, useState } from 'react'
+import { createContext, useEffect, useRef, useState } from 'react'
 import { postScore } from 'utils'
-
-export default function CreateScore({ showDialog, setShowDialog }) {
-  const contextValue = {
-    time,
-    timerRunning,
-    setTimerRunning
-  }
-  const scoreContext = createContext()
-  const [modalActive, setModalActive] = useState(false)
+import { useTimer } from 'components/Context/TimerContext'
+import React from 'react'
+export default function CreateScore({
+  showDialog,
+  setShowDialog,
+  setGameActive
+}) {
+  const { time } = useTimer()
   const submitForm = async (e) => {
-    setTimerRunning(false)
-    const username = FormData.get('username')
     e.preventDefault()
-    await postScore(username, time)
+    const username = e.target.elements.username.value
+    postScore(username, time)
     setShowDialog(false)
+    setGameActive(true)
   }
+
   return (
-    <scoreContext.Provider value={contextValue}>
-      <div>
-        {/* w-300 h-600 name  */}
-        <dialog id="scoreModal">
-          <form action="" onSubmit={submitForm}>
-            <label htmlFor="">username</label>
-            <input type="text" />
-            <button id="submitModal">Submit</button>
-          </form>
-        </dialog>
-      </div>
-    </scoreContext.Provider>
+    <div className=" fixed left-1/2  top-1/2 mx-auto flex size-96 -translate-x-1/2 -translate-y-1/2 items-center justify-center ">
+      <dialog className=" [input] flex size-full flex-col items-center bg-[#1B1725] p-6 text-2xl text-white">
+        <h1 className="text-4xl font-bold">Save Score:</h1>
+        <form
+          action=""
+          onSubmit={submitForm}
+          className="my-5 flex size-full flex-col items-center *:p-4"
+        >
+          <label htmlFor="" className="text-3xl">
+            username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            required
+            placeholder="Enter your username"
+            className="text-black"
+          />
+          <button
+            id="submitModal"
+            className="my-12 rounded-md bg-blue-400 p-6 active:bg-blue-500 "
+          >
+            Submit
+          </button>
+        </form>
+      </dialog>
+    </div>
   )
 }
